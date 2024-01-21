ee6bd5b97c5f99a85754f967b159986673034ad5
Nadzib <nadzib22@gmail.com>
2024-01-21 13:18:50 +0700

    update otak dari video yang berbeda https://youtu.be/EExSSotojVI?si=_j6li3AvxY3coMYR

---
+++ src/main/kotlin/maxmint_int.kt
@@ -0,0 +1,20 @@
+import kotlin.math.max
+
+fun main() {
+    var nomor = 22
+    val maxint = Int.MAX_VALUE
+    val minint = Int.MIN_VALUE
+    println("MAX_$maxint MIN_$minint")
+
+    val maxbyte = Byte.MAX_VALUE
+    val minbyte = Byte.MIN_VALUE
+    println("MAX_$maxbyte MIN_$minbyte")
+
+    val maxshort = Short.MAX_VALUE
+    val minshort = Short.MIN_VALUE
+    println("MAX_$maxshort MIN_$minshort")
+
+    val maxlong = Long.MAX_VALUE
+    val minlong = Long.MIN_VALUE
+    println("MAX_$maxlong MIN_$minlong")
+}
---
+++ src/main/kotlin/opening.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var username:String = "nazhiba"
+    username = "bwangKEE"
+
+    var nomor:Int = 19
+    println("Hello $username umurmu sudah $nomor")
+
+}
